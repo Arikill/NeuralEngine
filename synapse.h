1f5ebbf54bc50f8a12a7bd71791de5f239b9601e
ralluri-utah <ralluri.utah@gmail.com>
2023-01-03 22:43:23 -0500

    refactor synapse to better relate to mat-dev

--- synapse.h
+++ synapse.h
@@ -20,31 +20,10 @@ public:
         this->refrc = refrc;
     }
 
-    std::vector<T> remove_sub_refractory_inputs(std::vector<T> &event_times) {
-        int nevents = event_times.size();
-        std::vector<T> filtered_times(1, 0);
-        filtered_times[0] = event_times[0];
-        for(int i = 1; i < nevents; i++) {
-            if ((event_times[i] - event_times[i-1]) > this->refrc) {
-                filtered_times.push_back(event_times[i]);
-            }
-        }
-        return filtered_times;
-    }
-
-    std::vector<T> get_event_times(std::vector<int> const &events) {
-        int nevents = events.size();
-        std::vector<T> event_times(nevents, 0.0);
-        for(int i = 0; i < nevents; i++) {
-            event_times[i] = static_cast<T>(events[i])/this->fs;
-        }
-        return event_times;
-    }
-
-    void alpha(T event_time, std::vector<T> &times, int ntimes, std::vector<T> &output) {
+    void alpha(int ntimes, T event_time, std::vector<T> &times, std::vector<T> &output) {
         std::vector<T> exponent(ntimes, 0.0);
         for(int t = 0; t < ntimes; t++) {
-            if (times[t] > event_time) {
+            if (times[t] >= event_time) {
                 exponent[t] = (times[t] - event_time)/this->tau;
             }
             output[t] = this->gain*exponent[t]*exp(1-exponent[t]);
@@ -54,7 +33,6 @@ public:
     std::vector<T> get_response(std::vector<int> &events, std::vector<T> &times) {
         int ntimes = times.size();
         auto event_times = this->get_event_times(events);
-        auto filtered_times = this->remove_sub_refractory_inputs(event_times);
         int nevents = filtered_times.size();
         std::vector<std::vector<T>> nres(nevents, std::vector<T>(ntimes, 0));
         std::vector<T> res(ntimes, 0.0);
